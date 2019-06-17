aa83aa09fe7a6963995e19e756aa47be4eb0a46c
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-06-17 16:34:04 +0200

    adopted acquire_xyz and file_xyz_to_disc functions from SurrogateAbstractBaseClass [ci skip]

--- R/Surrogate.R
+++ R/Surrogate.R
@@ -9,8 +9,9 @@
 
 #' @section Construction:
 #' ```
-#'   surr = Surrogate$new(oml_task_id = 31, base_learner = "regr.glmnet", eval_measure = "auc",
-#'                        param_names = "lambda", surrogate_learner = "regr.ranger")
+#'   surr = Surrogate$new(oml_task_id = 31, base_learner = "regr.glmnet",
+#'                        eval_measure = "auc", param_names = "lambda",
+#'                        surrogate_learner = "regr.ranger", data_source = "glmnet_sample.csv")
 #' ```
 #'
 #' @section Fields:
@@ -103,8 +104,9 @@
 #' @family Surrogate
 #' @export
 #' @examples
-#'   surr = Surrogate$new(oml_task_id = 31, base_learner = "regr.glmnet", eval_measure = "auc",
-#'                        param_names = "lambda", surrogate_learner = "regr.ranger")
+#'   surr = Surrogate$new(oml_task_id = 31, base_learner = "regr.glmnet",
+#'                        eval_measure = "auc", param_names = "lambda",
+#'                        surrogate_learner = "regr.ranger", data_source = "glmnet_sample.csv")
 
 Surrogate = R6Class("Surrogate",
   public = list(
@@ -125,13 +127,12 @@ Surrogate = R6Class("Surrogate",
 
     fail_handle = NULL,
     handle_prefix = ".",
+    data_source = NULL,
 
     scale_fun_pars = NULL,
 
-    data = NULL,
-
     initialize = function(oml_task_id, base_learner, eval_measure, surrogate_learner,
-          param_set, param_names, use_cache = TRUE, fail_handle, handle_prefix) {
+          param_set, param_names, use_cache = TRUE, fail_handle, handle_prefix, data_source) {
       self$oml_task_id = assert_int(oml_task_id)
 
       self$base_learner = mlr::checkLearner(base_learner)
@@ -151,6 +152,8 @@ Surrogate = R6Class("Surrogate",
         self$fail_handle = fail::fail(self$fail_path())
       else
         self$fail_handle = fail::fail(paste(self$handle_prefix, fail_handle, sep = "/"))
+
+      self$data_source = assert_string(data_source, null.ok = T)
     },
 
     print = function(...) {
@@ -171,29 +174,77 @@ Surrogate = R6Class("Surrogate",
       )
     },
 
-    acquire_rtask = function() private$acquire_object("rtask"),
+    acquire_rtask = function() self$acquire_object("rtask"),
     acquire_model = function() self$acquire_object("model"),
-    acquire_resample = function() private$acquire_object("resample"),
+    acquire_resample = function() self$acquire_object("resample"),
 
     acquire_object = function(id) {
-      if (is.null(self[[id]]))
+      if (!is.null(self[[id]]))
         return()
       # If not in cache, create the object and write it to disk
       if (!self[[sprintf("in_cache_%s", id)]] | is.null(self$fail_handle)) {
         print('Object not in cache')
-        #self[[sprintf("file_%s_to_disk", id)]]()
+        self[[sprintf("file_%s_to_disk", id)]]()
       }
-      #else {
-      #  self[[id]] = self$fail_handle$get(self[[sprintf("key_%s", id)]])
-      #}
+      else {
+        self[[id]] = self$fail_handle$get(self[[sprintf("key_%s", id)]])
+      }
     },
 
-    load_csv = function(filepath) {
-      # TODO: check if file exists
-      data = as.data.frame(fread(filepath))
+    file_model_to_disk = function() {
+      self$acquire_rtask()
+      catf("<Obtaining Model>")
+      self$model = train(self$surrogate_learner, self$rtask)
+      if (self$use_cache) self$fail_handle$put(keys = self$key_model, self$model)
+    },
+
+    file_resample_to_disk = function(cv = cv3, measures = rmse) {
+      self$acquire_rtask()
+      catf("<Obtaining Resampling>")
+      self$resample = resample(self$base_learner, self$rtask, cv, measures)
+      if (self$use_cache) self$fail_handle$put(keys = self$key_resample, self$resample)
+    },
+
+    file_rtask_to_disk = function() {
+      catf("<Obtaining Data>")
+      d = self$load_csv()
+
+      # Take all hyper params if none specified
+      if (is.null(self$param_names))
+        self$param_names = names(self$param_set)
+
+      # Check if param_names are contained in data
+      checkmate::assert_subset(self$param_names, colnames(d))
+
+      # In case no data exists, we always sample 0 performance
+      if (nrow(d) == 0L) {
+        d = ParamHelpers::generateGridDesign(res = 3L, self$param_set)
+        d = d[, self$param_names, drop = FALSE]
+
+        # Convert logicals/character to factors for learner
+        which.logical = sapply(d, function(x) is.logical(x) | is.character(x))
+        if (sum(which.logical > 0))
+          d = do.call("cbind", list(d[, !which.logical], lapply(d[, which.logical],
+                      function(x) as.factor(as.character(x)))))
+        d$performance = 0.0
+      }
+      # attributes(d$target) = NULL # https://github.com/imbs-hl/ranger/issues/354
+      catf("<Obtaining Task>")
+      tsk = makeRegrTask(id = self$key_rtask, data = d, target = "performance")
+      self$rtask = removeConstantFeatures(tsk)
+
+      if (self$use_cache) self$fail_handle$put(keys = self$key_rtask, self$rtask)
+    },
+
+    load_csv = function() {
+      if (file.exists(system.file("extdata", self$data_source, package = "surrogates"))) {
+        ds = system.file("extdata", self$data_source, package = "surrogates")
+      }
+      data = as.data.frame(fread(ds))
       data = data[data$task_id == self$oml_task_id, c(self$eval_measure, self$param_names)]
-      self$data = data
-      colnames(self$data) = c("performance", self$param_names)
+      colnames(data) = c("performance", self$param_names)
+
+      return(data)
     },
 
     # TODO: implement loading different file formats
