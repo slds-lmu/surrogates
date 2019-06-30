60d719266420f28d2ea65bc15afdf9462f23a31c
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-06-30 16:37:51 +0200

    adjusted tests [ci skip]

--- tests/testthat/test_surrogate.R
+++ tests/testthat/test_surrogate.R
@@ -1,30 +1,31 @@
 context("surrogate_class")
 
 test_that("initialization", {
+  ds = system.file("extdata", "glmnet_sample.csv", package = "surrogates")
+  ps = get_param_set("glmnet")
   surr = Surrogate$new(oml_task_id = 3, base_learner = "regr.glmnet", eval_measure = "auc",
-    param_names = "lambda", surrogate_learner = "regr.ranger", data_source = "glmnet_sample.csv")
+    param_set = ps, param_names = "lambda", surrogate_learner = "regr.ranger",
+    data_source = ds, load_fun = load_from_csv)
+
   expect_r6(surr)
+  expect_equal(colnames(surr$data), c("performance", surr$param_names))
 })
 
 test_that("print_fun", {
+  ds = system.file("extdata/", "glmnet_sample.csv", package = "surrogates")
+  ps = get_param_set("glmnet")
   surr = Surrogate$new(oml_task_id = 3, base_learner = "regr.glmnet", eval_measure = "auc",
-    param_names = "lambda", surrogate_learner = "regr.ranger", data_source = "glmnet_sample.csv")
+    param_set = ps, param_names = "lambda", surrogate_learner = "regr.ranger",
+    data_source = ds, load_fun = load_from_csv)
 
   expect_invisible(print(surr))
 })
 
-test_that("load_csv_fun", {
-  surr = Surrogate$new(oml_task_id = 3, base_learner = "regr.glmnet", eval_measure = "auc",
-    param_names = "lambda", surrogate_learner = "regr.ranger", data_source = "glmnet_sample.csv")
-
-  data = surr$load_csv()
-  expect_data_frame(data)
-  expect_equal(colnames(data), c("performance", surr$param_names))
-})
-
-test_that("acquire_object_fun", {
-  surr = Surrogate$new(oml_task_id = 3, base_learner = "regr.glmnet", eval_measure = "auc",
-    param_names = "lambda", surrogate_learner = "regr.ranger", data_source = "glmnet_sample.csv")
-
-  surr$acquire_model()
-})
+#test_that("acquire_object_fun", {
+#  ds = system.file("inst/extdata/", "glmnet_sample.csv", package = "surrogates")
+#  surr = Surrogate$new(oml_task_id = 3, base_learner = "regr.glmnet", eval_measure = "auc",
+#    param_names = "lambda", surrogate_learner = "regr.ranger",
+#    data_source = ds, load_fun = load_from_csv)
+#
+#  surr$acquire_model()
+#})
