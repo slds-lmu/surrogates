cb6e768d4b34cd5bca0479d9d85f53b67f565cc4
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-06-30 16:37:37 +0200

    implementation for loading other file formats [ci skip]

--- R/load_functions.R
+++ R/load_functions.R
@@ -1,10 +1,28 @@
+#' @title Reading data
+#'
+#' @description
+#' Functions to load data from different sources
+
+#' @export
+#' @param filepath :: `character()`\cr
 load_from_csv = function(filepath) {
   data = as.data.frame(fread(filepath))
-
   return(data)
 }
 
-# TODO: implement loading different file formats
-load_rds = function(filepath) {}
-load_arff = function(filepath) {}
-load_oml = function(){}
+#' @export
+#' @param filepath :: `character()`\cr
+load_rds = function(filepath) {
+  data = as.data.frame(readRDS(filepath))
+  return(data)
+}
+
+#' @export
+#' @param filepath :: `character()`\cr
+load_arff = function(filepath) {
+  data = as.data.frame(farff::readARFF(filepath))
+  return(data)
+}
+
+# TODO
+load_oml = function() {}
