c6a7d34c2884bf6fa6017026c33d50badf07086f
Bagrat Ter-Akopyan <bagrat.akopyan@gmail.com>
2019-06-30 20:56:03 +0200

    adjusted tests [ci skip]

--- tests/testthat/test_surrogate.R
+++ tests/testthat/test_surrogate.R
@@ -21,11 +21,12 @@ test_that("print_fun", {
   expect_invisible(print(surr))
 })
 
-#test_that("acquire_object_fun", {
-#  ds = system.file("inst/extdata/", "glmnet_sample.csv", package = "surrogates")
-#  surr = Surrogate$new(oml_task_id = 3, base_learner = "regr.glmnet", eval_measure = "auc",
-#    param_names = "lambda", surrogate_learner = "regr.ranger",
-#    data_source = ds, load_fun = load_from_csv)
-#
-#  surr$acquire_model()
-#})
+test_that("acquire_object_fun", {
+  ds = system.file("extdata/", "glmnet_sample.csv", package = "surrogates")
+  ps = get_param_set("glmnet")
+  surr = Surrogate$new(oml_task_id = 3, base_learner = "regr.glmnet", eval_measure = "auc",
+    param_set = ps, param_names = "lambda", surrogate_learner = "regr.ranger",
+    data_source = ds, load_fun = load_from_csv)
+
+  surr$acquire_rtask()
+})
