bc7405fd377a6220e2bdf344dd4328dc7ddc40e4
pfistfl <pfistererf@googlemail.com>
2019-10-16 11:57:55 +0200

    Refactoring, cut out code

--- R/Surrogate.R
+++ R/Surrogate.R
@@ -117,9 +117,7 @@ Surrogate = R6Class("Surrogate",
     base_learner = NULL,
     surrogate_learner = NULL,
     param_set = NULL,
-    use_cache = TRUE,
 
-    rtask = NULL,
     model = NULL,
     resample = NULL,
     scaler = NULL,
@@ -130,10 +128,10 @@ Surrogate = R6Class("Surrogate",
     data = NULL,
 
     param_names = NULL,
-    cst_performance_ = 0,
 
-    initialize = function(oml_task_id, base_learner, eval_measure, surrogate_learner,
-          param_set, use_cache = TRUE, save_path, data_source, load_fun, scaler = Scaler$new()) {
+    initialize = function(oml_task_id, base_learner, eval_measure, 
+      surrogate_learner, param_set, save_path, data_source, 
+      load_fun, resample = FALSE, scaler = Scaler$new(),) {
 
       # Info used for sub-setting the data:
       self$oml_task_id = assert_int(oml_task_id)
@@ -142,10 +140,8 @@ Surrogate = R6Class("Surrogate",
       self$scaler = assert_class(scaler, "Scaler")
       self$surrogate_learner = mlr::checkLearner(surrogate_learner)
 
-      self$use_cache = checkmate::assert_flag(use_cache)
-      if (!missing(save_path))
-        self$save_path = save_path
-      self$save_path = fail::fail(self$fail_path)
+      if (!missing(save_path)) self$save_path = save_path
+      self$save_path = fail::fail(self$fail_path())
       self$data_source = assert_string(data_source, null.ok = TRUE)
       self$load_fun = assert_function(load_fun)
 
@@ -155,7 +151,28 @@ Surrogate = R6Class("Surrogate",
     },
 
     train = function() {
-      self$acquire_model()
+
+      if (file.exists(self$data_source))
+        self$data = self$load_fun(self)
+      else stop("Input file doesn't exist!")
+
+      catf("<Obtaining Task>")
+      tsk = makeRegrTask(id = as.character(self$oml_task_id), data = as.data.frame(self$data), target = "performance")
+      self$rtask = removeConstantFeatures(tsk)
+
+      catf("<Obtaining Model>")
+      self$model = train(self$surrogate_learner, self$rtask)
+
+      catf("<Writing model to disk>")
+      self$save_path$put(keys = self$key_model, self$model)
+
+      if (self$resample) {
+        catf("<Obtaining Resampling>")
+        self$resample = resample(self$surrogate_learner, self$rtask, cv, measures)
+        catf("<Writing resample to disk>")
+        self$save_path$put(keys = self$key_resample, self$resample)
+      }
+
       invisible(self)
     },
 
@@ -170,111 +187,18 @@ Surrogate = R6Class("Surrogate",
     print = function(...) {
       catf("Surrogate for OML task <%i> for measure <%s> for BL <%s>",
         self$oml_task_id, self$measure_name, self$baselearner_name)
-      catf("RTask: %s", ifelse(is.null(self$rtask), "no", "yes"))
-      catf("Model: %s", ifelse(is.null(self$model), "no", "yes"))
+      catf("Model: %s", ifelse(is.null(self$model), "untrained", "trained"))
       catf("Performance: %s", ifelse(is.null(self$resample), "N/A", self$resample$aggr))
       invisible(self)
     },
 
-    # Object Getters ---------------------------------------------------------------------
-    acquire_rtask = function() self$acquire_object("rtask"),
-    acquire_model = function() self$acquire_object("model"),
-    acquire_resample = function() self$acquire_object("resample"),
-
-    acquire_object = function(id) {
-      if (!is.null(self[[id]]))
-        return()
-      # If not in cache, create the object and write it to disk
-      if (!self[[sprintf("in_cache_%s", id)]] | is.null(self$save_path)) {
-        catf('Object not in cache')
-        self[[sprintf("file_%s_to_disk", id)]]()
-      }
-      else {
-        self[[id]] = self$save_path$get(self[[sprintf("key_%s", id)]])
-      }
-    },
-
-    file_model_to_disk = function() {
-      self$acquire_rtask()
-      catf("<Obtaining Model>")
-      self$model = train(self$surrogate_learner, self$rtask)
-      catf("<Writing model to disk>")
-      if (self$use_cache) self$save_path$put(keys = self$key_model, self$model)
-    },
-
-    file_resample_to_disk = function(cv = cv3, measures = list(rmse, spearmanrho, kendalltau, expvar, timepredict)) {
-      self$acquire_rtask()
-      catf("<Obtaining Resampling>")
-      self$resample = resample(self$surrogate_learner, self$rtask, cv, measures)
-      catf("<Writing resample to disk>")
-      if (self$use_cache) self$save_path$put(keys = self$key_resample, self$resample)
-    },
-
-    file_rtask_to_disk = function() {
-      # Load the data
-      if (file.exists(self$data_source))
-        self$data = self$load_fun(self)
-      else stop("Input file doesn't exist!")
-
-      # In case no data exists, we always sample 0 performance
-      if (nrow(self$data) == 0L) {
-        warning("No rows found in data")
-        self$fit_constant_model()
-      }
-
-      # attributes(d$target) = NULL # https://github.com/imbs-hl/ranger/issues/354
-      catf("<Obtaining Task>")
-      tsk = makeRegrTask(id = as.character(self$oml_task_id), data = as.data.frame(self$data), target = "performance")
-      self$rtask = removeConstantFeatures(tsk)
-
-      if (self$use_cache) {
-        catf("<Writing task to disk>")
-        self$save_path$put(keys = self$key_rtask, self$rtask)
-      }
-    },
-
-    fit_constant_model = function() {
-      d = ParamHelpers::generateGridDesign(res = 3L, self$param_set)
-      d = d[, self$param_names, drop = FALSE]
-
-      # Convert logicals/character to factors for learner
-      which.logical = sapply(d, function(x) is.logical(x) | is.character(x))
-      if (sum(which.logical > 0))
-        d = do.call("cbind", list(d[, !which.logical], lapply(d[, which.logical],
-          function(x) as.factor(as.character(x)))))
-      d$performance = self$cst_performance_
-      return(d)
-    },
-
-    save = function(keep.model = FALSE, keep.task = FALSE) {
-      if (!keep.model) self$model = NULL
-      if (!keep.task) self$rtask = NULL
-      self$save_path$put(keys = self$key_class, self)
-    }
-  ),
-
-  active = list(
-    key_base = function() sprintf("%i_%s_%s",
-      self$oml_task_id, self$eval_measure, self$base_learner),
-    key_model = function() paste0("surr_model_", self$key_base),
-    key_rtask = function() paste0("surr_rtask_", self$key_base),
-    key_resample = function() paste0("surr_resample_", self$key_base),
-    key_class = function() paste0("surrogate_", self$key_base),
-
-    in_cache_rtask = function() self$key_rtask %in% self$save_path$ls(),
-    in_cache_model = function() self$key_model %in% self$save_path$ls(),
-    in_cache_resample = function() self$key_resample %in% self$save_path$ls(),
-
     fail_path = function() {
-      paste(self$save_path, "surrogates", self$base_learner,
-        paste0(self$surrogate_learner$short.name, "_surrogate"),
-        self$eval_measure, self$scaler$scaler_name, sep = "/"
+      paste(self$save_path, "/surrogate", self$base_learner,
+        self$eval_measure, self$scaler$scaler_name, 
+        paste0(self$surrogate_learner$short.name),
+        sep = "_"
       )
     },
-    cst_performance = function(val) {
-      if(missing(val)) self$cst_performance_ = val else self$cst_performance_
-    }
-  ),
 
-  private = list()
+  )
 )
--- R/SurrogateCollection.R
+++ R/SurrogateCollection.R
@@ -11,86 +11,28 @@
 #' @export
 SurrogateCollection = R6Class("SurrogateCollection",
   public = list(
-    surrogates = NULL,
-    oml_task_ids = NULL,
-    holdout_task_id = NULL,
-    active = NULL,
+    surrogate_dir = NULL,
 
-    initialize = function(surrogates) {
-      self$surrogates = surrogates
-      names(self$surrogates) = vcapply(surrogates, function(x) x$key_base)
-      self$oml_task_ids = vnapply(self$surrogates, function(x) x$oml_task_id)
-      self$active = rep(TRUE, length(self$surrogates))
+    initialize = function(surrogate_dir) {
+      self$surrogate_dir = assert_path_for_output(surrogate_dir)
     },
 
-    # Predict on a list of [newdata], named with [base_learners]
-    predict = function(newdata, oml_task_ids = NULL, base_learners = NULL, rescale = FALSE) {
-      assert_flag(rescale)
-      assert_list(newdata, names = "named")
-      assert_integerish(oml_task_ids, null.ok = TRUE)
-      assert_character(base_learners, null.ok = TRUE)
+    predict_atomic = function(newdata, oml_task_id, base_learner, eval_measure) {
+      assert_data_frame(newdata)
+      assert_integerish(oml_task_id)
+      assert_string(base_learner)
+      assert_string(eval_measure)
 
-      if (is.null(base_learners)) {
-        base_learners = setNames(names(newdata), names(newdata))
-      }
-      if (is.null(oml_task_ids)) {
-        oml_task_ids = self$oml_task_ids[self$active]
-      }
+      file_path = self$get_file_path(oml_task_id, base_learner, eval_measure)
+      assert_file_exists(file_path)
 
-      prds = setNames(lapply(seq_along(base_learners), function(bl) {
-        private$predict_bl(newdata[[bl]], oml_task_ids, base_learners[bl], rescale)
-      }), base_learners)
-
-      return(prds)
+      readRDS(file_path)$surrogate$predict(newdata)
     },
 
-    # Set a surrogate as hold out (so predict will not evaluate it)
-    set_holdout_task = function(oml_task_id) {
-      assert_subset(oml_task_id, self$oml_task_ids)
-      self$holdout_task_id = oml_task_id
-      self$active = !(self$oml_task_ids %in% oml_task_id)
-    },
-    # Predict on the held-out data.
-    evaluate_holdout_task = function(newdata) {
-      self$predict(newdata, self$holdout_task_id, NULL)
-    }
-  ),
-
-  private = list(
-    # Predict a single base learner
-    predict_bl = function(newdata, oml_task_ids = NULL, baselearner = NULL, rescale) {
-      use_surrogates = private$subset_surrogates(oml_task_ids, baselearner)
-      if (length(use_surrogates) == 0) warning("No surrogates to predict on!")
-      prds = lapply(self$surrogates[use_surrogates], function(x) x$predict(newdata, rescale))
-      prds = do.call("cbind", prds)
-      return(prds)
-    },
-    subset_surrogates = function(oml_task_ids, baselearner) {
-      assert_subset(oml_task_ids, unique(self$oml_task_ids))
-      assert_subset(baselearner, unique(self$base_learners))
-
-      # If null, active tasks else specific task
-      if (is.null(oml_task_ids))
-        use_task = which(self$oml_task_ids %in% self$oml_task_ids_active)
-      else use_task = which(self$oml_task_ids %in% oml_task_ids)
-
-      # If no base learner is given, all else only specific bl
-      if (is.null(baselearner)) use_bls  = seq_along(self$surrogates)
-      else use_bls = which(self$base_learners %in% baselearner)
-
-      # Return applicable surrogates
-      Reduce(intersect, list(use_task, use_bls))
-    }
-  ),
-
-  active = list(
-    base_learners = function() vcapply(self$surrogates, function(x) x$base_learner),
-    measures = function() vcapply(self$surrogates, function(x) x$eval_measure),
-    surrogate_learners = function() vcapply(self$surrogates, function(x) {x$surrogate_learner$short.name}),
-    scalings = function() {
-      scales = unique(sapply(self$surrogates, function(x) x$scaler$scaler_name))
-      assert_true(length(scales) == 1L)
-      return(scales)
+    get_file_path = function(oml_task_id, base_learner, eval_measure) {
+      paste(self$surrogate_dir, "/surrogate", base_learner,
+        eval_measure, self$scaler$scaler_name, 
+        paste0(self$surrogate_learner$short.name), ".rds",  sep = "_")
     }
   )
 )
